3938bb7cbf711224ca07cf1e0b37c74bcf1c5c22
Gulchatai <sgalinaval@gmail.com>
2023-02-03 17:38:50 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-       // task2();
+        task2();
       //  task3();
       //  task4();
       //  task5();
       //  task6();
-     //   task7();
-      //  task8();
+
     }
     public static  void task1() {
         System.out.println("Задача 1");
@@ -23,5 +22,22 @@ public class Main {
         System.out.println("Значение переменной " + "e" + " с типом " + "float" + " равно " + e);
         double h = 100.12345678999;
         System.out.println("Значение переменной " + "h" + " с типом " + "double" + " равно " + h);
+    }
+    public static  void task2() {
+        System.out.println("Задача 2");
+        float firstNumber = 27.12f;
+        System.out.println(firstNumber);
+        long secondNumber = 987678965549L;
+        System.out.println(secondNumber);
+        double thirdNumber = 2.786;
+        System.out.println(thirdNumber);
+        int fourthNumber = 569;
+        System.out.println(fourthNumber);
+        int fifthNumber = -159;
+        System.out.println(fifthNumber);
+        short sixthNumber = 27897;
+        System.out.println(sixthNumber);
+        byte seventhNumber = 67;
+        System.out.println(seventhNumber);
     }
 }
