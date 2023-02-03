bf07bcdf826babcc3382e8e196fd777c28a309c8
Gulchatai <sgalinaval@gmail.com>
2023-02-03 15:10:38 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание 3");
+        task1();
+       // task2();
+      //  task3();
+      //  task4();
+      //  task5();
+      //  task6();
+     //   task7();
+      //  task8();
+    }
+    public static  void task1() {
+        System.out.println("Задача 1");
+        int a = 1000000;
+        System.out.println("Значение переменной " + "a" + " с типом " + "int" + " равно " + a);
+        byte b = 100;
+        System.out.println("Значение переменной " + "b" + " с типом " + "byte" + " равно " + b);
+        short c = 10000;
+        System.out.println("Значение переменной " + "c" + " с типом " + "short" + " равно " + c);
+        long d = 100000000000L;
+        System.out.println("Значение переменной " + "d" + " с типом " + "long" + " равно " + d);
+        float e = 100.123456789f;
+        System.out.println("Значение переменной " + "e" + " с типом " + "float" + " равно " + e);
+        double h = 100.12345678999;
+        System.out.println("Значение переменной " + "h" + " с типом " + "double" + " равно " + h);
     }
 }
